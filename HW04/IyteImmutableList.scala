c46ffb734da03ce6a2ec6466ccf123b2215d2d6a
halilduruakan <halilduruakan@gmail.com>
2016-11-24 13:10:29 +0200

    Update IyteImmutableList.scala

--- HW04/IyteImmutableList.scala
+++ HW04/IyteImmutableList.scala
@@ -1,51 +1,37 @@
-class IyteMutableList {
-  class Node (itemValue: Int, nextItem: Node ){
-    val value = itemValue
-    var next = nextItem
-  }
-  var start: Node = null
-  var cache: Node = null
-
-  def add(value:Int) {
-    if(start == null){
-      start = new Node(value,null)
-      cache = start
-    }
-    else{
-      cache.next = new Node(value, null)
-      cache = cache.next
-
-
-    }
-  }
-
-  override def toString() : String={
-    var n = start;
-    var listToString = ""
-    while(n != null){
-      listToString = listToString + n.value +","
-      n = n.next
-    }
-    listToString.substring(0,listToString.length-1)
-  }
-
-  def contains(x:Int): Boolean ={
-    var temp = start
-
-    if (temp == null)  false
-    while(temp != null){
-      if(temp.value == x ) return true
-      else temp = temp.next
-    }
-    false
-
-  }
-  def isNull() : Boolean= {
-    start == null
-  }
+  sealed abstract class IyteImmutableList {
+  def add(x: Int): IyteImmutableList
+  def contains(x: Int):Boolean
+  def isNull():Boolean
 }
-object IyteMutableList {
 
-  def apply() = new IyteMutableList()
+ object Nul extends IyteImmutableList {
+  override def add(x: Int): IyteImmutableList = new Node(x, Nul)
+  override def toString: String = ""
+  override def contains(x: Int): Boolean = false
+  override def isNull():Boolean= true
+}
 
+case class Node(value: Int, next: IyteImmutableList) extends IyteImmutableList {
+  override def add(x: Int): IyteImmutableList =new Node(x, this)
+  override def toString: String = {
+    next match {
+      case Nul => value.toString
+      case _ => value + "," + next
+    }
+  }
+  override def contains(x: Int): Boolean = {
+    def nextNode(x: Int, next: IyteImmutableList): Boolean = {
+      next match {
+        case Node(value, nn) => if (value == x) true else nextNode(x, nn)
+        case Nul => false
+      }
+    }
+    nextNode(x, this)
+  }
+
+  override def isNull():Boolean= false
+}
+
+object IyteImmutableList {
+  def apply(): IyteImmutableList = Nul
 }
